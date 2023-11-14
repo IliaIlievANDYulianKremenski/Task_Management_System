1429ef447f2daeb378054b55c8498b1cf5150ecd
ilia Iliev <ilia.dimchev.iliev@gmail.com>
2023-11-14 19:35:51 +0200

    "ChangeBugSeverityCommand" and "ChangeBugStatusCommand" implemented.

--- src/com/iliailievyuliankremenskiood/taskmanagement/commands/actions/ChangeBugSeverityCommand.java
+++ src/com/iliailievyuliankremenskiood/taskmanagement/commands/actions/ChangeBugSeverityCommand.java
@@ -2,11 +2,27 @@ package com.iliailievyuliankremenskiood.taskmanagement.commands.actions;
 
 import com.iliailievyuliankremenskiood.taskmanagement.core.contracts.TeamManagementRepository;
 import com.iliailievyuliankremenskiood.taskmanagement.commands.contracts.Command;
+import com.iliailievyuliankremenskiood.taskmanagement.models.contracts.Bug;
+import com.iliailievyuliankremenskiood.taskmanagement.models.enums.bugrelatedtypes.BugPriorityType;
+import com.iliailievyuliankremenskiood.taskmanagement.models.enums.bugrelatedtypes.BugSeverityType;
+import com.iliailievyuliankremenskiood.taskmanagement.utils.ParsingHelpers;
+import com.iliailievyuliankremenskiood.taskmanagement.utils.ValidationHelpers;
 
 import java.util.List;
 
 public class ChangeBugSeverityCommand implements Command {
+
+    /** Command format: Change_Bug_Priority {bug ID} {new severity} */
+
+    /*<-------Constant(s)------->*/
+    private static final String INVALID_BUG_SEVERITY_MESSAGE = "Invalid value for Bug Severity: %s. Should be CRITICAL, MAJOR or MINOR.";
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
+
+    /*<-------Field(s)------->*/
+
     private final TeamManagementRepository teamManagementRepository;
+
+    /*<-------Constructor(s)------->*/
 
     public ChangeBugSeverityCommand(TeamManagementRepository teamManagementRepository) {
         this.teamManagementRepository = teamManagementRepository;
@@ -14,6 +30,21 @@ public class ChangeBugSeverityCommand implements Command {
 
     @Override
     public String execute(List<String> parameters) {
-        return null;
+        ValidationHelpers.validateArgumentsCount(parameters,EXPECTED_NUMBER_OF_ARGUMENTS);
+
+        int bugId = ParsingHelpers.parseInteger(parameters.get(0),"Bug ID");
+        BugSeverityType bugSeverityType = ParsingHelpers.parseEnum(parameters.get(1), BugSeverityType.class, String.format(INVALID_BUG_SEVERITY_MESSAGE));
+
+        Bug bug = teamManagementRepository.findBugById(bugId);
+
+        bug.changeBugSeverity(bugSeverityType);
+
+        return userOutput(bug);
+    }
+
+    /*<-------Helper Method(s)------->*/
+
+    private static String userOutput(Bug bug) {
+        return bug.getActivityHistory().get(bug.getActivityHistory().size() - 1);
     }
 }
--- src/com/iliailievyuliankremenskiood/taskmanagement/commands/actions/ChangeBugStatusCommand.java
+++ src/com/iliailievyuliankremenskiood/taskmanagement/commands/actions/ChangeBugStatusCommand.java
@@ -2,11 +2,29 @@ package com.iliailievyuliankremenskiood.taskmanagement.commands.actions;
 
 import com.iliailievyuliankremenskiood.taskmanagement.core.contracts.TeamManagementRepository;
 import com.iliailievyuliankremenskiood.taskmanagement.commands.contracts.Command;
+import com.iliailievyuliankremenskiood.taskmanagement.models.contracts.Bug;
+import com.iliailievyuliankremenskiood.taskmanagement.models.enums.bugrelatedtypes.BugSeverityType;
+import com.iliailievyuliankremenskiood.taskmanagement.models.enums.bugrelatedtypes.BugStatusType;
+import com.iliailievyuliankremenskiood.taskmanagement.utils.ParsingHelpers;
+import com.iliailievyuliankremenskiood.taskmanagement.utils.ValidationHelpers;
 
 import java.util.List;
 
 public class ChangeBugStatusCommand implements Command {
+
+    /** Command format: Change_Bug_Priority {bug ID} {new status} */
+
+    //TODO Ask Yuli how to implement this command. Will the user write status, or will just choose change and will be selected the alternative enum.
+
+    /*<-------Constant(s)------->*/
+    private static final String INVALID_BUG_STATUS_MESSAGE = "Invalid value for Bug Status: %s. Should be ACTIVE or DONE.";
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
+
+    /*<-------Field(s)------->*/
+
     private final TeamManagementRepository teamManagementRepository;
+
+    /*<-------Constructor(s)------->*/
 
     public ChangeBugStatusCommand(TeamManagementRepository teamManagementRepository) {
         this.teamManagementRepository = teamManagementRepository;
@@ -14,6 +32,21 @@ public class ChangeBugStatusCommand implements Command {
 
     @Override
     public String execute(List<String> parameters) {
-        return null;
+        ValidationHelpers.validateArgumentsCount(parameters,EXPECTED_NUMBER_OF_ARGUMENTS);
+
+        int bugId = ParsingHelpers.parseInteger(parameters.get(0),"Bug ID");
+        BugStatusType bugStatusType = ParsingHelpers.parseEnum(parameters.get(1), BugStatusType.class, String.format(INVALID_BUG_STATUS_MESSAGE));
+
+        Bug bug = teamManagementRepository.findBugById(bugId);
+
+        bug.changeBugStatus();
+
+        return userOutput(bug);
+    }
+
+    /*<-------Helper Method(s)------->*/
+
+    private static String userOutput(Bug bug) {
+        return bug.getActivityHistory().get(bug.getActivityHistory().size() - 1);
     }
 }
